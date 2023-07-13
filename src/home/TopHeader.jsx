b82f756a186e2530cabbaae6ee9f036346e79d2b
Mark Carrington <frumarkcarrington@gmail.com>
2023-07-13 13:41:55 +0100

    fixed the bug on the css font-family not showing and finished the home page top header

--- src/home/TopHeader.jsx
+++ src/home/TopHeader.jsx
@@ -5,27 +5,70 @@ import { NavLink } from "react-router-dom";
 function TopHeader() {
   return (
     <HeaderContainer>
-      <div className="logo">
-        <img src="../assets/images/logo.svg" alt="logo" />
+      <div className="inner-content">
+        <div className="logo">
+          <img src="/images/logo.svg" alt="logo" />
+        </div>
+        <nav>
+          <NavLink to="/" className="normal">
+            Home
+          </NavLink>
+          <NavLink to="/" className="normal">
+            About us
+          </NavLink>
+          <NavLink to="/" className="normal">
+            Services
+          </NavLink>
+          <NavLink to="/" className="contact-us">
+            Contact us
+          </NavLink>
+        </nav>
       </div>
-      <nav>
-        <NavLink to="/" className="normal">
-          Home
-        </NavLink>
-        <NavLink to="/" className="normal">
-          About us
-        </NavLink>
-        <NavLink to="/" className="normal">
-          Services
-        </NavLink>
-        <NavLink to="/" className="contact-us">
-          Contact us
-        </NavLink>
-      </nav>
     </HeaderContainer>
   );
 }
 
 export default TopHeader;
 
-const HeaderContainer = styled.header``;
+const HeaderContainer = styled.header`
+  position: absolute;
+  z-index: 3;
+  width: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  margin-top: 1cm;
+  .inner-content {
+    display: flex;
+    width: 80%;
+    justify-content: space-between;
+    align-items: center;
+    .logo {
+      width: 200px;
+      height: 54.032px;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+    nav {
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      width: 45%;
+      gap: 0px;
+      a {
+        text-decoration: none;
+        color: #fff;
+        font-family: Roboto Slab;
+        font-size: 18px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: normal;
+        font-weight: bold;
+      }
+    }
+  }
+`;
