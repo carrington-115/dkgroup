8bbbcd175480826905a9a46a0441a7b0ff5a4415
Mark Carrington <frumarkcarrington@gmail.com>
2023-07-16 17:36:39 +0100

    created the hamburger menu and made it dynamic with js states

--- src/contact/ContactHeader.jsx
+++ src/contact/ContactHeader.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import { GoLinkExternal } from "react-icons/go";
+import { useNavigate } from "react-router-dom";
 
 function ContactHeader() {
+  const [menu, setMenu] = useState(false);
+  const navigate = useNavigate();
   return (
-    <Container>
+    <Container show={menu}>
       <div className="inner-content">
-        <div className="logo">
+        <div className="logo" onClick={() => navigate("/")}>
           <img src="/images/logo.svg" alt="logo" />
         </div>
         <nav>
@@ -23,10 +29,42 @@ function ContactHeader() {
             Contact us
           </NavLink>
         </nav>
-        <div className="menu">
+        <div show={menu} onClick={() => setMenu(true)} className="menu">
           <img src="/images/home images/menu.svg" alt="" />
         </div>
       </div>
+      <div show={menu} className="backdrop" onClick={() => setMenu(false)} />
+
+      <HamburgerMenu show={menu}>
+        <div className="content">
+          <div className="icons">
+            <AiOutlineClose onClick={() => setMenu(false)} />
+          </div>
+          <div className="links">
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/aboutus">About us</NavLink>
+            <NavLink to="/services">Services</NavLink>
+            <NavLink to="/contactus">Contact us</NavLink>
+            <h1>Follow us</h1>
+            <a className="social-media" href="">
+              <p>Facebook</p>
+              <GoLinkExternal />
+            </a>
+            <a className="social-media" href="">
+              <p>Instagram</p>
+              <GoLinkExternal />
+            </a>
+            <a className="social-media" href="">
+              <p>Twitter</p>
+              <GoLinkExternal />
+            </a>
+            <a className="social-media" href="">
+              <p>Linkedin</p>
+              <GoLinkExternal />
+            </a>
+          </div>
+        </div>
+      </HamburgerMenu>
     </Container>
   );
 }
@@ -110,6 +148,72 @@ const Container = styled.header`
           color: #000225;
           border-color: #000225;
         }
+      }
+    }
+  }
+  .backdrop {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    z-index: ${(props) => (props.show ? 5 : 0)};
+    display: ${(props) => (props.show ? "block" : "none")};
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+`;
+
+const HamburgerMenu = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  width: 60%;
+  background-color: white;
+  z-index: ${(props) => (props.show ? 10 : 0)};
+  display: ${(props) => (props.show ? "flex" : "none")};
+  flex-direction: column;
+  padding: 1cm;
+  .content {
+    .icons {
+      width: 100%;
+      display: flex;
+      justify-content: flex-end;
+      svg {
+        padding: 0.1cm;
+        width: 48px;
+        height: 48px;
+        border-radius: 5px;
+        cursor: pointer;
+        &:hover {
+          background-color: rgba(0, 0, 0, 0.08);
+        }
+      }
+    }
+    .links {
+      display: flex;
+      flex-direction: column;
+      margin-top: 1cm;
+      gap: 5px;
+      a {
+        text-decoration: none;
+        font-size: 24px;
+        color: black;
+        &:hover {
+          font-weight: bold;
+        }
+      }
+      h1 {
+        border-bottom: 1px solid black;
+        margin-top: 0.5cm;
+        padding-bottom: 0.15cm;
+        font-size: 22px;
+      }
+      .social-media {
+        display: flex;
+        align-items: center;
+        font-size: 18px;
+        gap: 10px;
       }
     }
   }
--- src/home/TopHeader.jsx
+++ src/home/TopHeader.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import { GoLinkExternal } from "react-icons/go";
+import { useNavigate } from "react-router-dom";
 
 function TopHeader() {
+  const [menu, setMenu] = useState(false);
+  const navigate = useNavigate();
   return (
-    <HeaderContainer>
+    <HeaderContainer show={menu}>
       <div className="inner-content">
-        <div className="logo">
+        <div className="logo" onClick={() => navigate("/")}>
           <img src="/images/logo.svg" alt="logo" />
         </div>
         <nav>
@@ -23,10 +29,42 @@ function TopHeader() {
             Contact us
           </NavLink>
         </nav>
-        <div className="menu">
+        <div className="menu" onClick={() => setMenu(true)}>
           <img src="/images/home images/menu.svg" alt="" />
         </div>
       </div>
+      <div show={menu} className="backdrop" onClick={() => setMenu(false)} />
+
+      <HamburgerMenu show={menu}>
+        <div className="content">
+          <div className="icons">
+            <AiOutlineClose onClick={() => setMenu(false)} />
+          </div>
+          <div className="links">
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/aboutus">About us</NavLink>
+            <NavLink to="/services">Services</NavLink>
+            <NavLink to="/contactus">Contact us</NavLink>
+            <h1>Follow us</h1>
+            <a className="social-media" href="">
+              <p>Facebook</p>
+              <GoLinkExternal />
+            </a>
+            <a className="social-media" href="">
+              <p>Instagram</p>
+              <GoLinkExternal />
+            </a>
+            <a className="social-media" href="">
+              <p>Twitter</p>
+              <GoLinkExternal />
+            </a>
+            <a className="social-media" href="">
+              <p>Linkedin</p>
+              <GoLinkExternal />
+            </a>
+          </div>
+        </div>
+      </HamburgerMenu>
     </HeaderContainer>
   );
 }
@@ -113,6 +151,72 @@ const HeaderContainer = styled.header`
           color: white;
           border-color: white;
         }
+      }
+    }
+  }
+  .backdrop {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    z-index: ${(props) => (props.show ? 5 : 0)};
+    display: ${(props) => (props.show ? "block" : "none")};
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+`;
+
+const HamburgerMenu = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  width: 60%;
+  background-color: white;
+  z-index: ${(props) => (props.show ? 10 : 0)};
+  display: ${(props) => (props.show ? "flex" : "none")};
+  flex-direction: column;
+  padding: 1cm;
+  .content {
+    .icons {
+      width: 100%;
+      display: flex;
+      justify-content: flex-end;
+      svg {
+        padding: 0.1cm;
+        width: 48px;
+        height: 48px;
+        border-radius: 5px;
+        cursor: pointer;
+        &:hover {
+          background-color: rgba(0, 0, 0, 0.08);
+        }
+      }
+    }
+    .links {
+      display: flex;
+      flex-direction: column;
+      margin-top: 1cm;
+      gap: 5px;
+      a {
+        text-decoration: none;
+        font-size: 24px;
+        color: black;
+        &:hover {
+          font-weight: bold;
+        }
+      }
+      h1 {
+        border-bottom: 1px solid black;
+        margin-top: 0.5cm;
+        padding-bottom: 0.15cm;
+        font-size: 22px;
+      }
+      .social-media {
+        display: flex;
+        align-items: center;
+        font-size: 18px;
+        gap: 10px;
       }
     }
   }
